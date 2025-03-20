a9d3e810a8d31c61d2f746079af80812e90d42dd
simonjaversek <simon.javersek@gmail.com>
2025-03-20 18:33:30 +0100

    Update LED.py

--- LED.py
+++ LED.py
@@ -20,25 +20,27 @@ def home():
     return render_template('index.html', led_state=led_state["status"])
 
 @application.route('/led/activate', methods=['POST'])
-@rate_limiter.limit("3 per second")
 def activate_led():
     global led_state
     GPIO.output(LED_CONTROL, GPIO.HIGH)
     led_state["status"] = "on"
-    return jsonify({"message": "LED vklopljena"})
+    return jsonify({"message": "LED vklopljena", "status": led_state["status"]})
 
 @application.route('/led/deactivate', methods=['POST'])
-@rate_limiter.limit("3 per second")
 def deactivate_led():
     global led_state
     GPIO.output(LED_CONTROL, GPIO.LOW)
     led_state["status"] = "off"
-    return jsonify({"message": "LED izklopljena"})
+    return jsonify({"message": "LED izklopljena", "status": led_state["status"]})
 
 @application.route('/button/check', methods=['GET'])
 def check_button():
     button_condition = GPIO.input(BUTTON_CONTROL)
     return jsonify({"message": "pritisnjeno" if button_condition == GPIO.LOW else "sprostjeno"})
+
+@application.route('/led/status', methods=['GET'])
+def led_status():
+    return jsonify({"status": led_state["status"]})
 
 if __name__ == '__main__':
     try:
